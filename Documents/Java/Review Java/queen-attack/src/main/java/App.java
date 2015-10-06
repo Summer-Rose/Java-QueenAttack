5dd13f9ea0d7e4cfed6b6f1c6d43510f9561ae23
Summer <sbrochtrup@gmail.com>
2015-10-05 22:27:44 -0700

    Update input form and display results

--- Documents/Java/Review Java/queen-attack/src/main/java/App.java
+++ Documents/Java/Review Java/queen-attack/src/main/java/App.java
@@ -18,17 +18,22 @@ public class App {
 			return new ModelAndView(model, layout);
 		}, new VelocityTemplateEngine());
 
-        // post("/queen-attack", (request, response) -> {
-        //     HashMap<String, Object> model = new HashMap<String, Object>();
-        //     String position1 = 
-        //     Boolean canAttack = queenAttack(position1, position2);
-        //     model.put("canAttack", canAttack);
-        //     model.put("template", "templates/index.vtl");
-        //     return new ModelAndView(model, layout);
-        // }, new VelocityTemplateEngine());
+        post("/queen-attack", (request, response) -> {
+            HashMap<String, Object> model = new HashMap<String, Object>();
+            String queenx = request.queryParams("queenx");
+            String queeny = request.queryParams("queeny");
+            String opponentx = request.queryParams("opponentx");
+            String opponenty = request.queryParams("opponenty");
+            Integer queen = Integer.parseInt(queenx.concat(queeny));
+            Integer opponent = Integer.parseInt(opponentx.concat(opponenty));
+            Boolean canAttack = queenAttack(queen, opponent);
+            model.put("canAttack", canAttack);
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 	}
 
-	public Boolean queenAttack(Integer position1, Integer position2) {
+	public static Boolean queenAttack(Integer position1, Integer position2) {
     	String position1Str = String.valueOf(position1);
         String position2Str = String.valueOf(position2);
         if (position1Str.substring(1,2).equals(position2Str.substring(1,2))) {
@@ -42,7 +47,7 @@ public class App {
         }
     }
 
-    public Boolean isDiagonal(String position1, String position2) {
+    public static Boolean isDiagonal(String position1, String position2) {
         Integer pos1Num1 = Integer.parseInt(position1.substring(0,1));
         Integer pos1Num2 = Integer.parseInt(position1.substring(1,2));
         Integer pos2Num1 = Integer.parseInt(position2.substring(0,1));
